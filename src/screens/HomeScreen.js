5e5a7fe1eb57580c5128f0485799057a7c73a9d1
praveen <mudadlapraveenkumar143@gmai.com>
2022-03-23 15:10:32 +0530

    using ternary operator

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,7 +2,7 @@ import React, { useEffect, useReducer, useState } from 'react';
 import { Link } from 'react-router-dom';
 // import data from '../data';
 import axios from 'axios';
-import logger  from "use-reducer-logger"
+import logger from 'use-reducer-logger';
 
 const reducer = (state, action) => {
   const { type, payload } = action;
@@ -44,42 +44,36 @@ const HomeScreen = () => {
     <div>
       <h1>feture products</h1>
       <div className="products">
-       
-       {loading ? <div>Loading...</div>:error?<div>{error}</div>:
-
-        
-        products.map((product) => (
-          <div key={product.slug} className="product">
-            <Link to={`/product/${product.slug}`}>
-              <img
-                src={product.image}
-                alt={product.name}
-                style={{ width: '250px', height: '250px' }}
-              />
-            </Link>
-            <div className="product-info">
+        {loading ? (
+          <div>Loading...</div>
+        ) : error ? (
+          <div>{error}</div>
+        ) : (
+          products.map((product) => (
+            <div key={product.slug} className="product">
               <Link to={`/product/${product.slug}`}>
-                <p>{product.name}</p>
+                <img
+                  src={product.image}
+                  alt={product.name}
+                  style={{ width: '250px', height: '250px' }}
+                />
               </Link>
-              <Link to={`/product/${product.slug}`}>
-                <p>
-                  <strong>${product.price}</strong>
-                </p>
-              </Link>
-              <button>Add to cart</button>
+              <div className="product-info">
+                <Link to={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </Link>
+                <Link to={`/product/${product.slug}`}>
+                  <p>
+                    <strong>${product.price}</strong>
+                  </p>
+                </Link>
+                <button>Add to cart</button>
+              </div>
             </div>
-            
-          </div>
-        
-        ))}
+          ))
+        )}
         }
-     
       </div>
-   
-    
-       
-
-      
     </div>
   );
 };
